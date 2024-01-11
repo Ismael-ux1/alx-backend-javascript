cb9f42e76aa068c67a7edad5b6404dc49b3dee1a
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-01-11 00:18:28 +0000

    String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -55,3 +55,42 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200)); // Outputs: Teacher
 console.log(createEmployee(1000)); // Outputs: Director
 console.log(createEmployee('$500')); // Outputs: Director
+
+
+
+
+// Define the isDirector function
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// Define the executeWork function
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+// Test the functions
+console.log(executeWork(createEmployee(200))); // Outputs: Getting to work
+console.log(executeWork(createEmployee(1000))); // Outputs: Getting to director tasks
+
+
+
+// Define the Subjects type
+type Subjects = 'Math' | 'History';
+
+// Define the teachClass function
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  }
+}
+
+// Test the function
+console.log(teachClass('Math')); // Outputs: Teaching Math
+console.log(teachClass('History')); // Outputs: Teaching History
