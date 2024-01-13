9d6e332efcd15d70f6164228ad35697dfe757317
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-01-13 21:03:55 +0000

    updated Throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,11 +1,11 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-    try {
-      let value = mathFunction();
-        queue.push(value);
-    } catch (error) {
-        queue.push(error.toString());
-    }
+  try {
+    const value = mathFunction();
+    queue.push(value);
+  } catch (error) {
+    queue.push(error.toString());
+  }
   queue.push('Guardrail was processed');
   return queue;
 }
