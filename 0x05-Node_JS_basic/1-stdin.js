e934a3fe6816508379a7b16d226ccb2064976d21
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-03-01 23:55:10 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,10 +3,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
-  const input = data.trim();
-  if (input) {
-    process.stdout.write(`Your name is: ${input}\n`);
-  }
+  const input = data.toString().trim();
+  process.stdout.write(`Your name is: ${input}\n`);
+  process.exit();
 });
 
 process.on('exit', () => {
