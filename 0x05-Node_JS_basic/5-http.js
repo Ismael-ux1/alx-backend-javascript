2ffc579636bc75be8770aae09fbc2a1e372a2baa
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-03-02 02:06:32 +0000

    updated complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,35 +3,36 @@ const fs = require('fs').promises;
 
 async function countStudents(path) {
   try {
-    const data = await fs.readFile(path, 'utf8');
-    const lines = data.split('\n');
-    const students = lines.filter((line) => line).map((line) => line.split(','));
-    const numberOfStudents = students.length;
+    const db = await fs.readFile(path, 'utf8');
+    const students = db.split('\n').filter((line) => line.trim() !== '');
+    const totalStudents = students.length - 1;
 
     const fields = {};
-    for (let i = 0; i < students.length; i += 1) {
-      const field = students[i][3];
-      if (!fields[field]) {
-        fields[field] = [];
+    students.slice(1).forEach((student) => {
+      const [name, , , field] = student.split(',').map((item) => item.trim());
+      if (field) {
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(name);
       }
-      fields[field].push(students[i][0]);
+    });
+
+    let studentList = `Number of students: ${totalStudents}\n`;
+    for (const fieldName in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, fieldName)) {
+        studentList += `Number of students in ${fieldName}: ${fields[fieldName].length}. List: ${fields[fieldName].join(', ')}\n`;
+      }
     }
 
-    let output = `Number of students: ${numberOfStudents}\n`;
-    for (const field in fields) {
-      if (Object.prototype.hasOwnProperty.call(fields, field)) {
-        output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-      }
-    }
-    return output;
+    return studentList;
   } catch (err) {
     throw new Error('Cannot load the database');
   }
 }
 
 const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
