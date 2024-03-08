a276ccaf667adf5d70cddf413b8eecafcd0c6571
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-03-08 21:11:02 +0000

    Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -14,21 +14,6 @@ app.get('/cart/:id', (req, res) => {
   }
 });
 
-app.get('/available_payments', (req, res) => {
-  res.json({
-    payment_methods: {
-      credit_cards: true,
-      paypal: false
-    }
-  });
-});
-
-app.use(express.json());
-app.post('/login', (req, res) => {
-  const username = req.body.userName;
-  res.send(`Welcome ${username}`);
-});
-
 app.listen(7865, () => {
   console.log('API available on localhost port 7865');
 });
