359dc3e32b2a821fe27450227f566c131e61f467
Ismael-ux1 <Ismaelabdulahi1@gmail.com>
2024-03-21 16:06:35 +0000

    Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building.js';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Call the parent class's constructor with sqft
+
+    if (typeof floors !== 'number') {
+      throw TypeError('Floors must be a number');
+    }
+
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
+
+export default SkyHighBuilding;
