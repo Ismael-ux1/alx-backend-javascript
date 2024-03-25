74b235647d02c123e5bc3e42eebde606867f6bba
Ismael-ux1 <Ismaelabdulahi1@gmail.com>
2024-03-25 19:23:39 +0000

    Vroom

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -6,7 +6,28 @@ class Car {
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    const symbolBrand = Symbol('brand');
+    const symbolMotor = Symbol('motor');
+    const symbolColor = Symbol('color');
+
+    return new this.constructor({
+      [symbolBrand]: this._brand,
+      [symbolMotor]: this._motor,
+      [symbolColor]: this._color,
+    });
+  }
+
+  // Getters for controlled access to private properties
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
   }
 }
 
